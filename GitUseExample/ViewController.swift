74ae2099a78df6665a93bc2900e3fffee5e5124e
Nguyen Hung <hung0974@gmail.com>
2019-03-12 23:19:46 +0700

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Nguyễn Hùng on 3/12/19.
+//  Copyright © 2019 Quantizen Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
